c32540552a7d33adfc9cb793ec6e5a68435b8246
ntphat2909 <109291235+ntphat2909@users.noreply.github.com>
2023-11-10 15:56:03 +0700

    hinhtron.py
    khối tròn có lưới

--- hinhtron.py
+++ hinhtron.py
@@ -1,33 +1,90 @@
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
-from math import *
-def draw_circle(radius, center):
-    glBegin(GL_TRIANGLE_FAN)
-    glVertex3fv(center)
-    for i in range(0, 361):
-        glVertex3fv((center[0] + radius * cos(radians(i)), center[1] + radius * sin(radians(i)), 0))
+import math
+
+vertices = []
+edges = []
+
+def create_sphere(radius, vertical_slices, horizontal_slices):
+    for i in range(horizontal_slices + 1):
+        h_angle = 2 * math.pi * i / horizontal_slices
+        h_y = radius * math.cos(h_angle)
+        h_radius = radius * math.sin(h_angle)
+
+        for j in range(vertical_slices + 1):
+            v_angle = math.pi * j / vertical_slices
+            x = h_radius * math.cos(v_angle)
+            y = h_y
+            z = h_radius * math.sin(v_angle)
+
+            vertices.append((x, y, z))
+
+    # Add edges for vertical circles
+    for i in range(horizontal_slices):
+        for j in range(vertical_slices):
+            edges.append((i * (vertical_slices + 1) + j, (i + 1) * (vertical_slices + 1) + j))
+
+    # Add edges for horizontal circles
+    for i in range(vertical_slices):
+        for j in range(horizontal_slices):
+            edges.append((j * (vertical_slices + 1) + i, j * (vertical_slices + 1) + i + 1))
+
+def draw_sphere():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
     glEnd()
+
+def set_projection():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-2, 2, -2, 2, -2, 2)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
 
 def main():
     pygame.init()
     display = (800, 800)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    pygame.display.set_caption("Hình tròn 3D")
+    pygame.display.set_caption("Sphere with Circles")
 
-    center = (0, 0, 0)
-    radius = 0.6
+    set_projection()
 
-    while True: 
+    create_sphere(radius=1, vertical_slices=15, horizontal_slices=15)
+
+    angle_x = 0
+    angle_y = 0
+    is_rotating = False
+    last_x = 0
+    last_y = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 return
+            if event.type == MOUSEBUTTONDOWN and event.button == 1:
+                is_rotating = True
+                last_x, last_y = pygame.mouse.get_pos()
+            if event.type == MOUSEBUTTONUP and event.button == 1:
+                is_rotating = False
+
+        if is_rotating:
+            x, y = pygame.mouse.get_pos()
+            delta_x = x - last_x
+            delta_y = y - last_y
+            angle_x += delta_x
+            angle_y += delta_y
+            last_x, last_y = x, y
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
+        glRotatef(angle_x, 0, 1, 0)
+        glRotatef(angle_y, 1, 0, 0)
 
-        draw_circle(radius, center)
+        draw_sphere()
 
         pygame.display.flip()
         pygame.time.wait(10)
